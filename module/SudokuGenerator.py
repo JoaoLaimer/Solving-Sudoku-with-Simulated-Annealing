6119696294572f18546ae1f73d1b7c058bb72a3f
JoaoLaimer <jvlaimer@gmail.com>
2024-03-10 18:05:27 -0300

    new gen

--- module/SudokuGenerator.py
+++ module/SudokuGenerator.py
@@ -14,86 +14,80 @@ from module.DisplayMatrix  import DisplayMatrix    as DM
 from module.CheckMatrix    import CheckMatrix      as CKM
 """
 from save.SaveSudoku       import SaveSudoku       as SAVE
-
+from random import sample
 class Sudoku:
 
     def __init__ ( self, SIZE, PERCENTAGE = 50 ):
         self.PERCENTAGE = PERCENTAGE
         self.SIZE = SIZE
-        self.grid = [ [ 0 for x in range( SIZE ) ] for y in range( SIZE ) ]
+        self.grid = [ ]
         self.numbers_list = list( range( 1, SIZE + 1 ) )
         self.box_size = int( SIZE ** 0.5 )
 
         start_time = time.time() 
-        self.InitializeGrid()
-        while( not self.FillGrid() ):
-            self.InitializeGrid()
         
+        rows = []
+        cols = []
+
+        rBase = range(self.box_size)
+
+        for g in self.shuffle(rBase):
+            for r in self.shuffle(rBase):
+                rows.append(g * self.box_size + r)
+
+        for g in self.shuffle(rBase):
+            for c in self.shuffle(rBase):
+                cols.append(g * self.box_size + c)
+
+        nums  = self.shuffle(range(1,self.SIZE+1))
+
+        for r in rows:
+            row = []
+            for c in cols:
+                number = nums[self.pattern(r, c)]
+                row.append(number)
+            self.grid.append(row)
+        
+        print(self.CheckSudoku())
+
+        self.InsertZeros()
+
         self.DisplayGrid()
+
+        
 
         end_time = time.time() 
         time_taken = end_time - start_time
 
         print( f"Time taken to generate the sudoku: {time_taken:.5f} seconds" )
-  
-    def InitializeGrid( self ):
-        for row in range( 0, self.SIZE ):
-            for col in range( 0, self.SIZE ):
-                self.grid[ row ][ col ] = 0
 
-    def CheckGrid( self, number, column, row ):
+    def shuffle(self,s): return sample(s,len(s)) 
+    def pattern(self,row,col): 
+        return (self.box_size*(row%self.box_size)+row//self.box_size+col)%self.SIZE
 
+    def CheckSudoku(self):
+        numberSet = set( range( 1, self.SIZE + 1 ) )
         for i in range( 0, self.SIZE ):
-            if self.grid[ i ][ column ] == number:
+            rowSet = set( self.grid[ i ] )
+            if rowSet != numberSet:
                 return False
             
-        for j in range(0, self.SIZE ):
-            if self.grid[ row ][ j ] == number:
+            columnSet = set( self.grid[ j ][ i ] for j in range( 0, self.SIZE ) )
+            if columnSet != numberSet:
                 return False
-            
-        column_check = column - column % self.box_size
-        row_check    = row - row % self.box_size
-        for i in range( 0, self.box_size ):
-            for j in range( 0, self.box_size ):
-                if self.grid[ i + row_check ][ j + column_check ] == number:
+        
+        for i in range(0,self.box_size):
+            for j in range(0,self.box_size):
+                boxSet = set()
+                for k in range(0,self.box_size):
+                    for l in range(0,self.box_size):
+                        boxSet.add(self.grid[i*self.box_size+k][j*self.box_size+l])
+                if boxSet != numberSet:
                     return False
-    
+
         return True
-
-    def FillGrid( self ):
-        for row in range( self.SIZE ):
-            full_numbers = list( range( 1, self.SIZE + 1 ) )
-            self.numbers_list = full_numbers
-            column = 0
-            infLoop = 0
-            while column < self.SIZE:
-                
-                if self.grid[ row ][ column ] == 0:
-                    number_dice_roll = random.randint( 1, 6 )
-                    number = random.choice( self.numbers_list )
-
-                    loopCount = 0
-                    while True:
-                        number = random.choice( self.numbers_list )
-                        if self.CheckGrid( number, column, row) :
-                            self.grid[ row ][ column ] = number
-                            self.numbers_list.remove( number )
-                            break
-                        elif loopCount > 30:
-                            for i in range( 0, self.SIZE ):
-                                self.grid[ row ][ i ] = 0
-                            column = -1
-                            self.numbers_list = list( range( 1, self.SIZE + 1 ) )
-                            break
-
-                        loopCount += 1
-
-                    infLoop += 1
-                    if infLoop > 10000:
-                        print( "Infinit loop" )
-                        return False
-                column += 1
-            
+    def InsertZeros( self ):
+   
         for i in range( 0, self.SIZE ):
             for j in range( 0, self.SIZE ):
                 number_dice_roll = random.randint( 1, 100 )
@@ -139,19 +133,18 @@ def SolveSudoku():
     pass
 
 if __name__ == "__main__":
-    PERCENTAGE = 85
+    PERCENTAGE = 100
+    
     while True:
         print( "1. Generate a new Sudoku\n2. Exit" )
         option = int( input( "Enter your choice: " ) )
         if option == 1:
             SIZE = int( input( "Enter the size N of the sudoku (NxN): " ) )
-            sudoku = Sudoku( SIZE, 85 )
+            sudoku = Sudoku( SIZE, PERCENTAGE )
             S = SAVE( SIZE, sudoku.grid )
-            S.SaveSudokuIncomplete( 85 )
+            S.SaveSudokuIncomplete( PERCENTAGE )
         elif option == 2:
             print( "Exiting..." )
             break
         else:
-            print( "Invalid option. Please choose again." )
-
-                        
+            print( "Invalid option. Please choose again." )            
