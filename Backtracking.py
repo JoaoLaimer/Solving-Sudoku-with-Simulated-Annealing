7619869623ecf700317960d706476f1930090137
Vinícius <viniciushallmann2015@gmail.com>
2024-03-11 15:37:59 -0300

    commit att backtracking

--- Backtracking.py
+++ Backtracking.py
@@ -6,7 +6,16 @@ import time
 import random
 import sys
 sys.setrecursionlimit(1500)
-SIZE = 25
+SIZE = 16
+
+def CountSudokus():
+    file  = open(f"sudokus\\sudoku_incomplete_{str(SIZE)}.txt", "r" )
+    count = 0
+    for line in file:
+        if line.startswith( "Sudoku" ):
+            count += 1
+    file.close()
+    return count
 
 def Randomsudoku():
     file  = open(f"sudokus\\sudoku_incomplete_{str(SIZE)}.txt", "r" )
@@ -18,8 +27,7 @@ def Randomsudoku():
     file.close()
     return random_sudoku_id
 
-def GetSudoku():
-    sudoku_id = Randomsudoku()
+def GetSudoku(sudoku_id):
     file      = open( f"sudokus\\sudoku_incomplete_{str(SIZE)}.txt", "r" )
     sudoku    = []
     for line in file:
@@ -40,41 +48,36 @@ def GenerateSudoku():
     M.FillGrid()
     end_time   = time.time() 
     time_taken = end_time - start_time
-    S = SAVE( SIZE, M.grid )
-    S.SaveSudokuWithHiddenNumbers( 15 )
+    #S = SAVE( SIZE, M.grid )
+    #S.SaveSudokuWithHiddenNumbers( 15 )
     D = DM( SIZE, M.grid )
     D.DisplayGrid()
     print( "\n" )
 
-def SolveSudoku():
+def SolveSudoku(id):
     
-    sudoku = GetSudoku()
-    D = DM( SIZE, sudoku )
-    D.DisplayGrid()
-    print( "\n" )
+    sudoku = GetSudoku(id)
+
     M = CM( SIZE )
+
     start_time = time.time()
     solved = M.Solve( sudoku )
     end_time = time.time()
+
     time_taken = end_time - start_time
-    print( f"Time taken to solve the sudoku: {time_taken:.5f} seconds" )
+
+
     D = DM( SIZE, solved )
     D.DisplayGrid()
     checker = CKM( SIZE, solved )
-    print( f"Is the solution valid? {checker.IsValidSolution()}" )
+    print( f"Sudoku ID: {id} | Is the solution valid? {checker.IsValidSolution()} | Time taken to solve the sudoku: {time_taken:.10f} seconds" )
+    print("------------------------------------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
 
-    while True:
-        print( "1. Generate a new Sudoku\n2. Solve a Sudoku\n3. Exit" )
-        option = int( input( "Enter your choice: " ) )
-        if option == 1:
-            GenerateSudoku()
-        elif option == 2:
-            SolveSudoku()
-        elif option == 3:
-            print( "Exiting..." )
-            break
-        else:
-            print( "Invalid option. Please choose again." )
+    count = CountSudokus()
+
+    for i in range( 10 ):
+        SolveSudoku(7)
+        print( "\n" )
